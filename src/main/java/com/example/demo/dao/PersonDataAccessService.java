7196ece0888bcd639cb38229bcdeaf8ed559805c
Kiyotaka <t10kiyotaka@gmail.com>
2020-12-21 20:15:42 +0900

    Add PersonDataAccessService

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,34 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao {
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return Collections.singletonList(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+}
